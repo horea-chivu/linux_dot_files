bad07542aa9fc89a663d27f9b8ce97bc224ff569
horea-chivu <horea.chivu@gmail.com>
2016-04-16 16:26:48 +0300

    Added Composer

--- fedora23.sh
+++ fedora23.sh
@@ -8,6 +8,11 @@ sudo rm fedy-installer
 sudo dnf install -y vlc vim-enhanced vim-X11 git clementine deluge emacs gcc-c++ make menulibre
 sudo dnf groupinstall -y "Development Tools" "Development Libraries"
 
+#Install NodeJS
 su -c "curl -sL https://rpm.nodesource.com/setup_5.x | bash -"
 sudo dnf install -y nodejs
 
+# Install Composer
+curl -sS https://getcomposer.org/installer | php
+$ sudo mv composer.phar /usr/local/bin/composer
+# $ sudo /usr/local/bin/composer --self-update
